b52d8e4ac1ac70489c3cc1a43ab25340689c3acc
longnn198 <longnn@twendeesoft.com>
2023-06-07 23:33:35 +0700

    send email

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello React, test send email
+          Hello React, test send email 2
         </a>
         <p className="App-link">Testing CI-CD, hope success</p>
       </header>
